c209937275e85b182ebca724abc06b026cda8a6c
Jonathan Schall <62366496+JaySchall@users.noreply.github.com>
2023-04-10 16:37:49 -0400

    Save and remove recipe button both implemented

--- Meal_Backend/recipes/urls.py
+++ Meal_Backend/recipes/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('about/', views.about, name="Recipe_About"),
     path('account/', views.account, name="Recipe_Account"),
     path('<name>/', views.recipeItem, name="Recipe_Item"),
-
+    path('<name>/save/', views.addRecipe, name="Recipe_Save"),
+    path('<name>/del/', views.delRecipe, name="Recipe_Delete"),
 ]
--- Meal_Backend/recipes/views.py
+++ Meal_Backend/recipes/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.hashers import make_password
 from .forms import registerForm, loginForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
-from .models import Users, Recipe
+from .models import Users, Recipe, Link
 # Create your views here.
 def home(request):
     recipes = Recipe.objects.all()
@@ -63,16 +63,56 @@ def about(request):
     return render(request, 'AboutPage.html')
 
 def account(request):
+    
     if request.user.is_authenticated:
-        return render(request, 'UserPage.html')
+        linked = Link.objects.filter(email=request.user.id)
+        print(linked)
+        recipes = []
+        for id in linked:
+            print(id)
+            recipes.append(Recipe.objects.get(id=id.id.id))
+        context = {
+        "recipes": recipes,
+    }
+        return render(request, 'UserPage.html', context)
     else:
         return redirect("../login")
 
 def recipeItem(request, name):
     recipe = Recipe.objects.get(name=name)
+    link = Link.objects.filter(id=recipe.id, email=request.user.id)
+    print(link)
     context = {
         'recipe': recipe,
+        'link': link,
     }
     return render(request, 'recipe.html', context)
 
+def addRecipe(request, name):
+    recipe = Recipe.objects.get(name=name)
+    if request.user.is_authenticated:
+        try:
+            existing = Link.objects.get(id=recipe, email=request.user)
+            print(existing)
+        except:
+            
+            print(request.user.email +" " + str(recipe.id))
+            linked = Link.objects.create(id=recipe, email=request.user)
+            return redirect("../")
 
+        print("exists already")
+        return redirect("../")
+    else:
+        return redirect("../")
+def delRecipe(request, name):
+    recipe = Recipe.objects.get(name=name)
+    if request.user.is_authenticated:
+        try:
+            existing = Link.objects.get(id=recipe, email=request.user)
+            existing.delete()
+            return redirect("../")
+        except:
+            print("Can't remove what isn't real")
+            return redirect("../")
+    else:
+        return redirect("../")
